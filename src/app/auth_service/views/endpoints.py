f1de3e19560ff5f7f5042ead0297fa0f02c710e4
BeFunny1 <befunny2@vm-9a2b5c80.na4u.ru>
2021-08-19 07:44:54 +0000

    rename dir

--- src/app/auth_service/views/endpoints.py
+++ src/app/auth_service/views/endpoints.py
@@ -1,16 +1,16 @@
-from fastapi import Depends, APIRouter
+from app.auth_service.models import AuthenticatedUser
+from app.auth_service.utils.password_hash import hash_password
+from fastapi import Depends, APIRouter, HTTPException
 from starlette import status
 from starlette.responses import Response
 
-from app.auth_service.views.models.update import UpdateTokensInput, UpdateTokensOutput
-from app.auth_service.views.models.deregister import DeregisterInput, DeregisterOutput
-from app.auth_service.views.models.register import RegisterInput, RegisterOutput
+from app.auth_service.views.in_out_models.update import UpdateTokensInput, UpdateTokensOutput
+from app.auth_service.views.in_out_models.deregister import DeregisterInput, DeregisterOutput
+from app.auth_service.views.in_out_models.register import RegisterInput, RegisterOutput
 from app.auth_service.views.middlewares import check_api_secret
 
-router = APIRouter(
-    prefix='/auth_service',
-    dependencies=[Depends(check_api_secret)]
-)
+
+router = APIRouter(prefix='/auth_service', dependencies=[Depends(check_api_secret)])
 
 
 @router.post(
@@ -18,15 +18,24 @@ router = APIRouter(
     response_model=RegisterOutput,
 )
 async def register(body: RegisterInput):
-    return Response(status_code=status.HTTP_200_OK)
+    await AuthenticatedUser.create(
+        login=body.login,
+        password_hash=hash_password(body.password),
+        refresh_token='plug_refresh_token'
+    )
+    return Response(status_code=201)
 
 
-@router.post(
+@router.delete(
     '/deregister',
     response_model=DeregisterOutput,
 )
 async def deregister(body: DeregisterInput):
-    return Response(status_code=status.HTTP_200_OK)
+    user = await AuthenticatedUser.filter(login=body.login).delete()
+    if hash_password(body.password) == user.password_hash:
+        # user.delete()
+        return Response(status_code=status.HTTP_200_OK)
+    raise HTTPException(status_code=400, detail="Incorrect password")
 
 
 @router.post(
