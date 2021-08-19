09169fb109fb57daee7e6c06ee99a9350b491f3a
BeFunny1 <befunny2@vm-9a2b5c80.na4u.ru>
2021-08-19 07:47:21 +0000

    add postgreSQL, tortoise ORM

--- Makefile
+++ Makefile
@@ -10,7 +10,8 @@ down:
 	docker-compose down
 
 piplock:
-	docker-compose run --rm --no-deps --volume=${PWD}/src:/app/src --workdir=/app/src app pipenv install
+	docker-compose run --rm --no-deps --volume=${PWD}/src:/src --workdir=/src auth_service pipenv install
 	sudo chown -R ${USER} src/Pipfile.lock
 
-
+psql:
+	docker exec -it db psql -U postgres
--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,16 @@
-import uvicorn
-
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from config.settings import *
 from app.auth_service.views.endpoints import router as auth
 
 auth_service = FastAPI()
 auth_service.include_router(auth, tags=['auth_service'])
+
+register_tortoise(
+    auth_service,
+    db_url=os.getenv("DATABASE_URL"),
+    modules={"models": ["app.auth_service.models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
