3bfb26f458ce8581718f1d675c02731d4843391e
BeFunny1 <befunny2@vm-9a2b5c80.na4u.ru>
2021-08-24 07:27:09 +0000

    refactor code, start implemented jwt

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,8 +1,15 @@
 import os
 
+from pydantic import BaseModel
+
+
 ACCESS_TOKEN_TTL = 15
 REFRESH_TOKEN_TTL = 12 * 24 * 60
 
 API_SECRET = os.environ['API_SECRET']
-JWT_SECRET = os.environ['JWT_SECRET']
+# JWT_SECRET = os.environ['JWT_SECRET']
 HASH_SALT = os.environ['HASH_SALT']
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = os.environ['JWT_SECRET']
