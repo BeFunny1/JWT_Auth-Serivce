ae4365aa88233cfdc2207500f52296ef390adbb1
BeFunny1 <befunny2@vm-9a2b5c80.na4u.ru>
2021-08-24 07:24:22 +0000

    refactor model, because refresh_tokens will store in redis

--- src/app/auth_service/models.py
+++ src/app/auth_service/models.py
@@ -5,7 +5,6 @@ from tortoise import fields
 class AuthenticatedUser(Model):
     login = fields.TextField(pk=True)
     password_hash = fields.TextField()
-    refresh_token = fields.TextField()
     
     def __str__(self) -> str:
         return self.login
